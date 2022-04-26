fc8306e71daa620f4d1a061956b5f948619cb491
Kyle KIM <k2wackie@gmail.com>
2022-04-26 22:49:47 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const app = express();
-const port = 5000;
+
+const config = require("./config/key");
+
+const { User } = require("./model/User");
+
+//application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+//application/json
+app.use(express.json());
 
 const mongoose = require("mongoose");
 
@@ -11,15 +19,22 @@ const mongoDB_ID = process.env.DB_USER;
 const mongoDB_PW = process.env.DB_PSWORD;
 
 mongoose
-  .connect(
-    `mongodb+srv://${mongoDB_ID}:${mongoDB_PW}@boilerplate.gpqid.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("mongoDB Connected..."))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.send("h"));
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
+});
+
 app.listen(PORT, () => console.log(`listening on port ${PORT}!`));
