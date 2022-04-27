2610860eb5bb89c3e7b32fa1960eb770dc9458c0
Kyle KIM <k2wackie@gmail.com>
2022-04-27 16:18:51 +0900

    Add redux & antd CSS framwork & concurrently

--- client/src/index.js
+++ client/src/index.js
@@ -4,17 +4,33 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import "antd/dist/antd.css";
+import { applyMiddleware, createStore } from "redux";
+import promiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
+import Reducer from "./_reducers/index";
+
+const createStoreWithMiddleware = applyMiddleware(
+  promiseMiddleware,
+  ReduxThunk
+)(createStore);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider
+        store={createStoreWithMiddleware(
+          Reducer,
+          window.__REDUCER_DEVTOOLS_EXTENSION__ &&
+            Window.__REDUCER_DEVTOOLS_EXTENSION__()
+        )}
+      >
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
