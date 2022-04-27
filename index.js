57eb6108673e7f682f8560c757500acb5e98cf2c
Kyle KIM <k2wackie@gmail.com>
2022-04-27 09:41:35 +0900

    Add Route of login & auth

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
 const express = require("express");
-const dotenv = require("dotenv");
 const app = express();
-
 const config = require("./config/key");
-
+const cookieParser = require("cookie-parser");
+const { auth } = require("./middleware/auth");
 const { User } = require("./model/User");
 
 //application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 //application/json
 app.use(express.json());
-
+app.use(cookieParser());
 const mongoose = require("mongoose");
 
-dotenv.config();
 const PORT = process.env.PORT || 5000;
-const mongoDB_ID = process.env.DB_USER;
-const mongoDB_PW = process.env.DB_PSWORD;
 
 mongoose
   .connect(config.mongoURI, {
@@ -28,12 +24,51 @@ mongoose
 
 app.get("/", (req, res) => res.send("h"));
 
-app.post("/register", (req, res) => {
+app.post("/api/user/register", (req, res) => {
   const user = new User(req.body);
 
   user.save((err, userInfo) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({ success: true });
+  });
+});
+
+app.post("/api/user/login", (req, res) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다.",
+      });
+    }
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 틀렸습니다.",
+        });
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+        res
+          .cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userID: user._id });
+      });
+    });
+  });
+});
+
+// role 0 -> 일반유저,  role 1 -> 관리자
+app.get("/api/user/auth", auth, (req, res) => {
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image,
   });
 });
 
